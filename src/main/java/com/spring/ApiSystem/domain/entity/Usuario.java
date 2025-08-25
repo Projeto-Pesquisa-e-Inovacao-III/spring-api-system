677f9cf405a772f029e0c619bd7911eba6a7a221
VitorSuave <vitor.rodrigues@sptech.school>
2025-08-25 14:24:37 -0300

    feat/adicionado a entidade usuario

--- src/main/java/com/spring/ApiSystem/domain/entity/Usuario.java
+++ src/main/java/com/spring/ApiSystem/domain/entity/Usuario.java
@@ -1,4 +1,32 @@
 package com.spring.ApiSystem.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private Long cpf;
+
+    @Column(nullable = false)
+    private String senha;
+    private String nome;
+
+    private boolean ativo = true;
+
 }
