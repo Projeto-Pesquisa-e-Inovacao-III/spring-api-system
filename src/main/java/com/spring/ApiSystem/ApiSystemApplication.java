f148302a5d4a14703011ae800a99ad207d284894
VitorSuave <vitor.rodrigues@sptech.school>
2025-08-25 14:00:25 -0300

    chore/atualizando a estrutura do projeto

---
+++ src/main/java/com/spring/ApiSystem/ApiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ApiSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/ApiSystem/ApiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ApiSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
