c1918125979e8618c6065cac613725649eae6d57
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2025-09-22 20:09:24 -0300

    fix(Arquitetura,UsuárioRepository): Ajustando arquitetura e repository
    Co-authored-by: FabioHKC <fabio.cesare@sptech.school>
    Co-authored-by: FillipeMendesC <fillipemcoelho@hotmail.com>

--- src/main/java/com/spring/ApiSystem/repository/UsuarioRepository.java
+++ src/main/java/com/spring/ApiSystem/repository/UsuarioRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UsuarioRepository {
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByEmail(String email);
     Optional<Usuario> findByCpf(String cpf);
