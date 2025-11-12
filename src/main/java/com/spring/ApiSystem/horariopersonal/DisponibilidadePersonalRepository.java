3d7320ac14103f65b01409096ad76d815d637968
VitorSuave <vitor.rodrigues@sptech.school>
2025-11-12 19:02:35 -0300

    feat(disponibilidadePersonal):Realizado um select no banco para retornar os dias disponiveis e os horarios do personal. Assim a gente mentem menos chamadas ao banco.

--- src/main/java/com/spring/ApiSystem/horariopersonal/DisponibilidadePersonalRepository.java
+++ src/main/java/com/spring/ApiSystem/horariopersonal/DisponibilidadePersonalRepository.java
@@ -29,4 +29,13 @@ public interface DisponibilidadePersonalRepository extends JpaRepository<Disponi
             @Param("horaFim") LocalTime horaFim,
             @Param("horarioId") Long horarioId
     );
+
+    @Query("SELECT d FROM DisponibilidadePersonal d " +
+            "WHERE d.personal.id = :personalId AND d.diaSemana IN :dias " +
+            "ORDER BY d.diaSemana, d.horaInicio")
+    List<DisponibilidadePersonal> findByPersonalIdAndDiaSemanaInOrderByDiaSemanaHoraInicio(
+            @Param("personalId") Long personalId,
+            @Param("dias") List<DiaSemana> dias
+    );
+
 }
