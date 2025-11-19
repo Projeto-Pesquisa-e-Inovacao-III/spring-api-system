cd3e0cfd57e3ae32353b51f40f47198bc8ff5838
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2025-11-18 22:45:25 -0300

    fix(AgendamentoService): Adicionando DTos

---
+++ src/main/java/com/spring/ApiSystem/agendamento/dto/request/ReqRegistrarAusenciaAgendamentoPersonal.java
@@ -0,0 +1,6 @@
+package com.spring.ApiSystem.agendamento.dto.request;
+
+public record ReqRegistrarAusenciaAgendamentoPersonal(
+        Long idAgendamento,
+        String descricaoCancelamento)
+{}
