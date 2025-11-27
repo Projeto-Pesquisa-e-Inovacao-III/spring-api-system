f7ea6c3cee4df9631d87c34733b4665fbc8bd31b
Joao Victor Galdino Cunha <jvcunha1@stefanini.com>
2025-11-27 20:17:59 -0300

    fix - adição do Tipo Produto enum

---
+++ src/main/java/com/spring/ApiSystem/personal/dto/response/ResBuscarTodosPersonalDTO.java
@@ -0,0 +1,21 @@
+package com.spring.ApiSystem.personal.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.spring.ApiSystem.telefone.dto.response.ResListarTelefonesPorIdDoUsuario;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record ResBuscarPersonalPorIdDTO(
+        Long id,
+        String nome,
+        String sexo,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        LocalDate dataNascimento,
+        String email,
+        String cref,
+        boolean ativo,
+        String caminhoFoto,
+        List<ResListarTelefonesPorIdDoUsuario> telefones
+) {}
+
---
+++ src/main/java/com/spring/ApiSystem/produtoexibicao/enums/TipoProduto.java
@@ -0,0 +1,4 @@
+package com.spring.ApiSystem.produtoexibicao.enums;
+
+public class TipoProduto {
+}
