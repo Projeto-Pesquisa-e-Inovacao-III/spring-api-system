6545b62927f0202fe612c12eadae05ce4dcd61d0
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2025-11-29 21:12:27 -0300

    feat:  aniversario do personal

--- src/main/java/com/spring/ApiSystem/personal/dto/response/ResListarPersonaisDTO.java
+++ src/main/java/com/spring/ApiSystem/personal/dto/response/ResListarPersonaisDTO.java
@@ -1,7 +1,10 @@
 package com.spring.ApiSystem.personal.dto.response;
 
+import java.time.LocalDate;
+
 public record ResListarPersonaisDTO(
     Long id,
     String nome,
+    LocalDate dataNascimento,
     String caminhoFoto
 ) { }
