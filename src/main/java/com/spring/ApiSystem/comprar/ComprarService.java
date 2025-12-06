914b52219f2c2af43bc7caca35ddda0a489545dc
FillipeMendesC <fillipemcoelho@hotmail.com>
2025-12-05 21:48:23 -0300

    fix - melhorias dockerizacao

--- src/main/java/com/spring/ApiSystem/comprar/ComprarService.java
+++ src/main/java/com/spring/ApiSystem/comprar/ComprarService.java
@@ -3,6 +3,7 @@ package com.spring.ApiSystem.comprar;
 import com.spring.ApiSystem.aluno.Aluno;
 import com.spring.ApiSystem.comprar.dto.LinkDto;
 import com.spring.ApiSystem.usuario.security.JpaUserDetailsService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -19,6 +20,9 @@ public class ComprarService {
     private final JpaUserDetailsService userDetailsService;
     private final RestTemplate restTemplate;
 
+    @Value("${pag.url}")
+    private String url;
+
     public ComprarService(JpaUserDetailsService userDetailsService, RestTemplate restTemplate) {
         this.userDetailsService = userDetailsService;
         this.restTemplate = restTemplate;
@@ -31,21 +35,15 @@ public class ComprarService {
 
         Long userId = userDetailsService.getCurrentAluno().getId();
 
-
         Map<String, Long> requestBody = new HashMap<>();
         requestBody.put("user_id", userId);
         requestBody.put("produto_exibicao_id", produtoExibicaoId);
 
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-
         HttpEntity<Map<String, Long>> requestEntity = new HttpEntity<>(requestBody, headers);
 
-
-        String url = "http://localhost:8081/checkouts/simple";
-
-        return restTemplate.postForEntity(url, requestEntity, LinkDto.class).getBody();
+        return restTemplate.postForEntity(url+"/checkouts/simple", requestEntity, LinkDto.class).getBody();
     }
 }
