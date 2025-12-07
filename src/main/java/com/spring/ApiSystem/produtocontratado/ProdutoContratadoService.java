5e28dd7b0282ab769aaf2e3934df59b327355a80
FillipeMendesC <fillipemcoelho@hotmail.com>
2025-12-07 12:48:45 -0300

    fix - melhorias dockerizacao

--- src/main/java/com/spring/ApiSystem/produtocontratado/ProdutoContratadoService.java
+++ src/main/java/com/spring/ApiSystem/produtocontratado/ProdutoContratadoService.java
@@ -117,7 +117,7 @@ public class ProdutoContratadoService {
     @Transactional
     public Long decrementar(Long alunoId, TipoAula tipoAula) {
         ProdutoContratado produtoContratado = produtoContratadoRepository
-                .findFirstByAlunoIdAndTipoAulaWithSaldoGreaterThanOne(
+                .findFirstByAlunoIdAndTipoAulaComSaldoMaiorQueZero(
                        alunoId, tipoAula
                 ).orElseThrow(UsuarioSemTipoAulaException::new);
 
@@ -165,8 +165,6 @@ public class ProdutoContratadoService {
 
 
     public List<ResProdutoContratadoDto> listarPorAluno(Pageable pageable, String nomeProduto, LocalDate dataInicio, LocalDate dataFim){
-
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: "+dataFim);
         List<ProdutoContratado> produtosContratados = produtoContratadoRepository.findByAlunoIdWithFilters(
                 detailsService.getCurrentAluno(),
                 nomeProduto,
