894b6f54f4c16fc12a4ec4a0b4c00a1a2682a3b1
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2026-03-04 15:15:12 -0300

    criando Cpf

--- src/main/java/com/spring/ApiSystem/domain/aluno/Aluno.java
+++ src/main/java/com/spring/ApiSystem/domain/aluno/Aluno.java
@@ -1,6 +1,8 @@
 package com.spring.ApiSystem.domain.aluno;
 
 
+import com.spring.ApiSystem.domain.aluno.vo.Cpf;
+import com.spring.ApiSystem.domain.aluno.vo.CpfConverter;
 import com.spring.ApiSystem.domain.telefone.Telefone;
 import com.spring.ApiSystem.domain.usuario.Usuario;
 import com.spring.ApiSystem.domain.usuario.enums.TipoUsuario;
@@ -15,27 +17,26 @@ import java.time.Period;
 @PrimaryKeyJoinColumn(name = "id")
 public class Aluno extends Usuario {
 
-    @Column(unique = true)
-    private String cpf;
+    @Convert(converter = CpfConverter.class)
+    @Column(unique = true, nullable = false, length = 11)
+    private Cpf cpf;
 
     public Aluno() {
     }
 
     public Aluno(Long id, TipoUsuario tipo, String nome, String sexo, LocalDate dataNascimento, String email, String salt, String senha, boolean ativo, String caminhoFoto, List<Telefone> telefones, String cpf) {
         super(id, TipoUsuario.ALUNO, nome, sexo, dataNascimento, email, salt, senha, ativo, caminhoFoto, telefones);
-        this.cpf = cpf;
+        this.cpf = new Cpf(cpf);
     }
     public Integer getIdade() {
         return Period.between(getDataNascimento(), LocalDate.now()).getYears();
     }
 
-    public String getCpf() {
+    public Cpf getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
+    public void setCpf(Cpf cpf) {
         this.cpf = cpf;
     }
-
-
 }
