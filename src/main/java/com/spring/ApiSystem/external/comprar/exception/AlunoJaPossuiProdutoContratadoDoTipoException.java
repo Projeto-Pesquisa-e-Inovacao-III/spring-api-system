ee0c9645f44b5a5f472f5f08d0e12cf7385801c9
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2026-03-17 20:06:17 -0300

    fix:Nome da exception

--- src/main/java/com/spring/ApiSystem/external/comprar/exception/AlunoJaPossuiProdutoContratadoDoTipoException.java
+++ src/main/java/com/spring/ApiSystem/external/comprar/exception/AlunoJaPossuiProdutoContratadoDoTipoException.java
@@ -2,8 +2,9 @@ package com.spring.ApiSystem.external.comprar.exception;
 
 import com.spring.ApiSystem.domain.produtoexibicao.enums.TipoProduto;
 
-public class AlunoAlreadyHaveProdutoContratadoByTipoProdutoException extends RuntimeException {
-    public AlunoAlreadyHaveProdutoContratadoByTipoProdutoException(TipoProduto tipoProduto) {
+public class AlunoJaPossuiProdutoContratadoDoTipoException extends RuntimeException {
+    public AlunoJaPossuiProdutoContratadoDoTipoException(TipoProduto tipoProduto) {
         super("O Aluno já tem um produto contratado do tipo " + tipoProduto);
     }
 }
+
