f62eb66486b6b31fe946089b2570a026f28cb8e9
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-03-23 15:23:46 -0300

    add - validacao de senha

--- src/main/java/com/spring/ApiSystem/domain/aluno/dto/request/ReqCadastroAlunoDTO.java
+++ src/main/java/com/spring/ApiSystem/domain/aluno/dto/request/ReqCadastroAlunoDTO.java
@@ -4,6 +4,7 @@ import com.spring.ApiSystem.domain.telefone.dto.request.ReqCadastrarTelefoneDTO;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import java.util.Date;
@@ -22,7 +23,11 @@ public record ReqCadastroAlunoDTO(
     String email,
 
     @NotBlank(message = "A senha é obrigatória")
-    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    @Pattern(regexp = ".*[a-z].*", message = "A senha deve conter pelo menos uma letra minúscula")
+    @Pattern(regexp = ".*[A-Z].*", message = "A senha deve conter pelo menos uma letra maiúscula")
+    @Pattern(regexp = ".*\\d.*", message = "A senha deve conter pelo menos um número")
+    @Pattern(regexp = ".*[^a-zA-Z0-9].*", message = "A senha deve conter pelo menos um caractere especial")
+    @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
     String senha,
 
     @NotBlank(message = "O cpf é obrigatório")
