184899cfc7f46375d0d5979b102ced67573b7803
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2026-04-09 21:13:09 -0300

    fix

--- src/main/java/com/spring/ApiSystem/shared/infrastructure/rabbit/config/RabbitMqConfig.java
+++ src/main/java/com/spring/ApiSystem/shared/infrastructure/rabbit/config/RabbitMqConfig.java
@@ -1,5 +1,7 @@
 package com.spring.ApiSystem.shared.infrastructure.rabbit.config;
 
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -14,4 +16,17 @@ public class RabbitMqConfig {
     public MessageConverter messageConverter() {
         return new JacksonJsonMessageConverter();
     }
+
+        @Bean
+        public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+            SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+            factory.setConnectionFactory(connectionFactory);
+            factory.setMessageConverter(messageConverter());
+
+            factory.setErrorHandler(t -> {
+                System.err.println("ERRO RABBITMQ: Falha após 3 tentativas. Verifique se o container está rodando.");
+            });
+
+            return factory;
+        }
 }
