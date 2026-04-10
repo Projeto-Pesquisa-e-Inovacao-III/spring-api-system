93155a1d665d0feb123eead4a79fec29c9ff00ef
FabioHKC <fabio.cesare@sptech.school>
2026-04-09 21:05:14 -0300

    Added filter on Page<alunos>

--- src/main/java/com/spring/ApiSystem/domain/aluno/AlunoService.java
+++ src/main/java/com/spring/ApiSystem/domain/aluno/AlunoService.java
@@ -108,12 +108,7 @@ public class AlunoService {
     }
 
     public Page<ResListarAlunosDto> listarAlunos(Pageable pageable, String nome) {
-        if(nome != null){
-            Pageable firstPage = PageRequest.of(0, pageable.getPageSize());
-        }
-
         Page<Aluno> alunos = alunoRepository.findAllAtivosContainingNome(pageable, nome);
-
         return alunos.map(alunoMapper::toResListarAlunosDto);
     }
 
