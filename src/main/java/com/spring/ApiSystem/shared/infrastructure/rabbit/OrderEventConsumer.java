802222ac0f376223e1bd88b5392b987dfad1d6cf
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-04-14 11:00:35 -0300

    fix - readaptação para a nova api de pagamentos

--- src/main/java/com/spring/ApiSystem/shared/infrastructure/rabbit/OrderEventConsumer.java
+++ src/main/java/com/spring/ApiSystem/shared/infrastructure/rabbit/OrderEventConsumer.java
@@ -1,5 +1,8 @@
 package com.spring.ApiSystem.shared.infrastructure.rabbit;
 
+import com.spring.ApiSystem.domain.produtocontratado.ProdutoContratado;
+import com.spring.ApiSystem.domain.produtocontratado.ProdutoContratadoService;
+import com.spring.ApiSystem.external.comprar.ComprarService;
 import com.spring.ApiSystem.shared.infrastructure.rabbit.dto.OrderPaidMessage;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -8,8 +11,18 @@ import org.springframework.stereotype.Component;
 @Component
 @ConditionalOnProperty(prefix = "messaging.rabbitmq", name = "enabled", havingValue = "true")
 public class OrderEventConsumer {
+
+    private final ProdutoContratadoService produtoContratadoService;
+
+    public OrderEventConsumer(ProdutoContratadoService produtoContratadoService) {
+        this.produtoContratadoService = produtoContratadoService;
+    }
+
     @RabbitListener(queues = "api_system.orders.queue")
     public void consumeOrderPaidEvent(OrderPaidMessage message) {
-        System.out.println("Recebido: " + message.orderId());
+        produtoContratadoService.criarProdutoContratadoPeloIdAluno(
+                Long.parseLong(message.customerId()),
+                Long.parseLong((String) message.itensId().getFirst())
+        );
     }
 }
