c3561b068f0e90e741e056da5af24565be7783fd
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2026-04-28 22:43:20 -0300

    fix:apagando item não utilizado

--- src/main/java/com/spring/ApiSystem/external/comprar/exception/AlunoJaTemProdutoContratado.java
+++
@@ -1,7 +0,0 @@
-package com.spring.ApiSystem.external.comprar.exception;
-
-public class AlunoJaTemProdutoContratado extends RuntimeException {
-    public AlunoJaTemProdutoContratado() {
-        super("O aluno já tem um produto contratado do tipo pacote");
-    }
-}
