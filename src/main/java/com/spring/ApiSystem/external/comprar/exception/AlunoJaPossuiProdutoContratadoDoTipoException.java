0ea5b6a110ab44bc9016012d650ccf20e7578ad5
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2026-05-02 16:41:06 -0300

    fix: mensagem de erro

--- src/main/java/com/spring/ApiSystem/external/comprar/exception/AlunoJaPossuiProdutoContratadoDoTipoException.java
+++ src/main/java/com/spring/ApiSystem/external/comprar/exception/AlunoJaPossuiProdutoContratadoDoTipoException.java
@@ -4,7 +4,7 @@ import com.spring.ApiSystem.domain.produtoexibicao.enums.TipoProduto;
 
 public class AlunoJaPossuiProdutoContratadoDoTipoException extends RuntimeException {
     public AlunoJaPossuiProdutoContratadoDoTipoException(TipoProduto tipoProduto) {
-        super("Não é possível prosseguir: o aluno já possui um produto do tipo " + tipoProduto + ".");
+        super("Você já possui um pacote ativo. Para continuar agendando, adquira um pacote adicional.");
     }
 }
 
