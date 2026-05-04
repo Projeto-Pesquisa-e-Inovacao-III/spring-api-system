7a674043491c93e6e470fe199b9fa767de570c2f
João Victor Galdino <166883345+JoaoGaldinoCunha@users.noreply.github.com>
2026-05-03 23:46:38 -0300

    Fix/mensagem erro pacote aluno (#151)
    * fix:texto da exception
    
    * fix:apagando item não utilizado
    
    * fix: mensagem de erro

--- src/main/java/com/spring/ApiSystem/external/comprar/exception/AlunoJaPossuiProdutoContratadoDoTipoException.java
+++ src/main/java/com/spring/ApiSystem/external/comprar/exception/AlunoJaPossuiProdutoContratadoDoTipoException.java
@@ -4,7 +4,7 @@ import com.spring.ApiSystem.domain.produtoexibicao.enums.TipoProduto;
 
 public class AlunoJaPossuiProdutoContratadoDoTipoException extends RuntimeException {
     public AlunoJaPossuiProdutoContratadoDoTipoException(TipoProduto tipoProduto) {
-        super("O Aluno já tem um produto contratado do tipo " + tipoProduto);
+        super("Você já possui um pacote ativo. Para continuar agendando, adquira um pacote adicional.");
     }
 }
 
