398f26238bc96089b60340bb1370bb5ffcabf61a
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-05-05 16:14:32 -0300

    add - fix pr opção para parar rabbit em dev  (#154)

--- src/main/java/com/spring/ApiSystem/shared/infrastructure/rabbit/OrderEventConsumer.java
+++ src/main/java/com/spring/ApiSystem/shared/infrastructure/rabbit/OrderEventConsumer.java
@@ -1,16 +1,13 @@
 package com.spring.ApiSystem.shared.infrastructure.rabbit;
 
-import com.spring.ApiSystem.domain.produtocontratado.ProdutoContratado;
 import com.spring.ApiSystem.domain.produtocontratado.ProdutoContratadoService;
-import com.spring.ApiSystem.external.comprar.ComprarService;
 import com.spring.ApiSystem.shared.infrastructure.rabbit.dto.OrderPaidMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
-@Component
+@ConditionalOnProperty(name = "rabbitmq.enabled", havingValue = "true", matchIfMissing = true)
 public class OrderEventConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(OrderEventConsumer.class);
