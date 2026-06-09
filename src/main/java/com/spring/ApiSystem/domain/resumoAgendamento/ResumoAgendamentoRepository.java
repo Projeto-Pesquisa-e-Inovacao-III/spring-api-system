cdf31ede867a0138387ccfc60ea3e84027a4d716
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-06-09 19:19:13 -0300

    fix - order by desc

--- src/main/java/com/spring/ApiSystem/domain/resumoAgendamento/ResumoAgendamentoRepository.java
+++ src/main/java/com/spring/ApiSystem/domain/resumoAgendamento/ResumoAgendamentoRepository.java
@@ -33,7 +33,7 @@ public interface ResumoAgendamentoRepository extends JpaRepository<ResumoAgendam
     INNER JOIN FETCH r.aluno al
     INNER JOIN FETCH r.personal p
     WHERE al.id = :alunoId
-    ORDER BY a.data ASC
+    ORDER BY a.data DESC
     """)
     Page<ResumoAgendamento> getResumosWithAgendamentoByAlunoId(@Param("alunoId") Long alunoId, Pageable pageable);
 }
