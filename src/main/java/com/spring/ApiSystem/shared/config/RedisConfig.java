fbfc95706cb1c65983995da4608a1193ee195b4c
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-06-09 17:22:10 -0300

    add - default cache provider

--- src/main/java/com/spring/ApiSystem/shared/config/RedisConfig.java
+++ src/main/java/com/spring/ApiSystem/shared/config/RedisConfig.java
@@ -1,13 +1,16 @@
 package com.spring.ApiSystem.shared.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 import java.time.Duration;
 
+@Profile("prod")
 @Configuration
 public class RedisConfig {
 
